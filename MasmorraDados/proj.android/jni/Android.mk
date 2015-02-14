c973d2c4dc0907d19cc10df8a48c261467ecb6ed
Marlon Andrade <marlonmandrade@gmail.com>
2015-02-14 12:12:30 -0200

    Ajustando config de build do projeto do android
    - adicionando novos arquivos para serem buildados
    - adicionando diretorios para autoinclude

--- MasmorraDados/proj.android/jni/Android.mk
+++ MasmorraDados/proj.android/jni/Android.mk
@@ -12,9 +12,12 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+		   ../../Classes/scene/IntroScene.cpp \
+		   ../../Classes/scene/MainMenuScene.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+  		    $(LOCAL_PATH)/../../Classes/scene \
+		    $(LOCAL_PATH)/../../Classes/model
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
