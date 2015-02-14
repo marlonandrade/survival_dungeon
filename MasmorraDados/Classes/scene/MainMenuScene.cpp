f62a7fe7369137369351ae7757a4d64cb6635f0c
Marlon Andrade <marlonmandrade@gmail.com>
2015-02-14 11:09:20 -0200

    Classe 'vazia' para scena de menu inicial

---
+++ MasmorraDados/Classes/scene/MainMenuScene.cpp
@@ -0,0 +1,25 @@
+//
+//  MainMenuScene.cpp
+//  MasmorraDados
+//
+//  Created by Marlon Andrade on 14/02/2015.
+//
+//
+
+#include "MainMenuScene.h"
+
+USING_NS_CC;
+
+Scene* MainMenu::createScene()
+{
+    return NULL;
+}
+
+bool MainMenu::init()
+{
+    if (!Layer::init()) {
+        return false;
+    }
+    
+    return true;
+}
---
+++ MasmorraDados/Classes/scene/MainMenuScene.h
@@ -0,0 +1,23 @@
+//
+//  MainMenuScene.h
+//  MasmorraDados
+//
+//  Created by Marlon Andrade on 14/02/2015.
+//
+//
+
+#ifndef __MasmorraDados__MainMenuScene__
+#define __MasmorraDados__MainMenuScene__
+
+#include "cocos2d.h"
+
+class MainMenu : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+    virtual bool init();
+    
+    CREATE_FUNC(MainMenu);
+};
+
+#endif /* defined(__MasmorraDados__MainMenuScene__) */
